09e8316d76b7c91992d14c0d7c4fee29ec12fad3
Souvik Chatterjee <91524372+Bravoth0729v@users.noreply.github.com>
2022-01-10 13:48:05 +0530

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,4 +1,15 @@
-console.log("Hello Souvik");
-//java script keywords:
-// var , let , const
-console.clear("console clered")
+var fullname = "Souvik Chatterjee"
+console.log(fullname)
+console.clear("Console Cleared")
+var paymentmethod;
+paymentmethod = "Credit Card"
+console.log(paymentmethod)
+console.log(fullname)
+x = Boolean
+let loggedcount = 34;
+console.log(loggedcount)
+console.log(x)
+a = false
+console.log(a)
+// console.log(paymentmethod)
+// var paymentmethod;
---
+++ switchcase.js
@@ -0,0 +1,17 @@
+var login = 'Instagram'
+switch (login) {
+    case 'Google':
+        console.log("Allow The user to login");
+        break;
+    case 'Email':
+        console.log("Allow The user to login");
+        break;
+    case 'Facebook':
+        console.log("Allow The user to login");
+        break;
+
+
+    default:
+        console.log('The user is not allowed.');
+        break;
+}
